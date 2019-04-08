4427172318de367802e1dea6887e50d341d77c53
MelvinMuuo <muuomelvin73@gmail.com>
2019-04-08 11:41:06 +0300

    add indentation

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,29 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
+function equals() {
+  turnNumber1 = getRandomIntInclusive(1,6);
+  return turnNumber1;
+}
 function addition1() {
-  return turnNumber += getRandomIntInclusive(1,6);
+  return total1 = total1 + turnNumber1;
+}
+function equals2() {
+  turnNumber2 = getRandomIntInclusive(1,6);
+  return turnNumber2;
 }
 function addition2() {
-  return turnTotal += getRandomIntInclusive(1,6);
+  return total2 = total2 + turnNumber2;
 }
-
 
 $(document).ready(function() {
   $("#button1").click(function() {
-    $("#output").text(addition1());
+    $("#output").text(equals());
+    $("#output1b").text(addition1());
   })
   $("#button2").click(function() {
-    $("#output2").text(addition2());
+    $("#output2").text(equals2());
+    $("#output2b").text(addition2());
   })
   $("#hold1").click(function() {
     $("#button1").attr("disabled",true);
